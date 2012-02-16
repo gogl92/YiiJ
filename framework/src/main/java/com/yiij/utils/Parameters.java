24ed18e35b725fc6a3d54cc1c5baa850a59aa1e3
Rangel Reale <rangelspam@gmail.com>
2012-02-16 14:20:19 -0200

    - Trying to make JTME understand a simple form of function call (in progress)

--- framework/src/main/java/com/yiij/utils/Parameters.java
+++ framework/src/main/java/com/yiij/utils/Parameters.java
@@ -2,8 +2,50 @@ package com.yiij.utils;
 
 import java.util.HashMap;
 
+/**
+ * Helper class used as parameters (Map<String, Object>).
+ * Use {@link Builder} to create an instance easily 
+ * @author Rangel Reale
+ */
 @SuppressWarnings("serial")
 public class Parameters extends HashMap<String, Object>
 {
+	/**
+	 * Parameter builder class
+	 * @author Rangel Reale
+	 */
+	public static class Builder
+	{
+		private Parameters _result;
 
+		/**
+		 * Builder construtor.
+		 */
+		public Builder()
+		{
+			super();
+			_result = new Parameters();
+		}
+		
+		/**
+		 * Adds a new value to the resulting Parameters
+		 * @param name
+		 * @param value
+		 * @return
+		 */
+		public Builder add(String name, Object value)
+		{
+			_result.put(name,  value);
+			return this;
+		}
+		
+		/**
+		 * Returns the resulting built Parameters
+		 * @return
+		 */
+		public Parameters build()
+		{
+			return _result;
+		}
+	}
 }
--- framework/src/main/java/com/yiij/web/renderers/JTMERenderer.java
+++ framework/src/main/java/com/yiij/web/renderers/JTMERenderer.java
@@ -5,12 +5,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
+import com.floreysoft.jmte.DefaultModelAdaptor;
 import com.floreysoft.jmte.Engine;
+import com.floreysoft.jmte.ErrorHandler;
 import com.floreysoft.jmte.NamedRenderer;
+import com.floreysoft.jmte.ProcessListener;
+import com.floreysoft.jmte.Processor;
 import com.floreysoft.jmte.RenderFormatInfo;
+import com.floreysoft.jmte.TemplateContext;
+import com.floreysoft.jmte.token.StringToken;
+import com.floreysoft.jmte.token.Token;
 import com.yiij.base.interfaces.IContext;
 import com.yiij.web.BaseController;
 import com.yiij.web.WebApplicationComponent;
@@ -35,17 +43,27 @@ public class JTMERenderer extends WebApplicationComponent implements
 			Object data, boolean doReturn) throws IOException
 	{
 		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("this", controller);
+		model.put("this", new BaseControllerWrapper(controller));
 		model.put("data", data);
 		
 		Engine engine = new Engine();
 		engine.registerNamedRenderer(new HtmlEncodeRenderer());
+		engine.setModelAdaptor(new YiiJModelAdaptor());
 		if (doReturn)
 			return engine.transform(readTextFile(file), model);
 		webApp().getResponse().getWriter().print(engine.transform(readTextFile(file), model));
 		return null;
 	}
 
+	private static class EngineProcessListener implements ProcessListener
+	{
+
+		@Override
+		public void log(TemplateContext context, Token token, Action action)
+		{
+		}
+	}
+	
 	private String readTextFile(String file) throws IOException {
 	    StringBuilder result = new StringBuilder();
 
@@ -90,4 +108,102 @@ public class JTMERenderer extends WebApplicationComponent implements
 		}
 		
 	}
+	
+	private static interface MethodCallable
+	{
+		Object call(TemplateContext context, String method, Token token);
+	}
+	
+	public static class MethodCallableCall implements Processor<Object>
+	{
+		private MethodCallable _callable;
+		private String _method;
+		private Token _token;
+		
+		public MethodCallableCall(MethodCallable callable, String method, Token token)
+		{
+			super();
+			_callable = callable;
+			_method = method;
+			_token = token;
+			
+		}
+		
+		@Override
+		public Object eval(TemplateContext context)
+		{
+			return _callable.call(context, _method, _token);
+		}
+	}
+	
+	private static class YiiJModelAdaptor extends DefaultModelAdaptor
+	{
+		protected Object nextStep(Object o, String attributeName,
+				ErrorHandler errorHandler, Token token) {
+			if (o instanceof MethodCallable)
+			{
+				return new MethodCallableCall((MethodCallable)o, attributeName, token);
+			}
+			return super.nextStep(o, attributeName, errorHandler, token);
+		}
+	}
+	
+	public static class NullOutput
+	{
+		public String toString()
+		{
+			return "";
+		}
+	}
+	
+	
+	public static class BaseControllerWrapper implements MethodCallable
+	{
+		private BaseController _controller;
+		
+		public BaseControllerWrapper(BaseController controller)
+		{
+			super();
+			_controller = controller;
+		}
+		
+		@Override
+		public Object call(TemplateContext context, String method, Token token)
+		{
+			try
+			{
+				// widget must always capture output
+				if (method.equals("beginWidget"))
+				{
+					_controller.webApp().getResponse().ob_start();
+					_controller.beginWidget(((StringToken)token).getDefaultValue());
+					return new NullOutput();
+				}
+				else if (method.equals("endWidget"))
+				{
+					_controller.endWidget(((StringToken)token).getDefaultValue());
+					_controller.webApp().getResponse().ob_end_clean();
+					return new NullOutput();
+				}
+				else if (method.equals("widget"))
+				{
+					//String[] params = ((StringToken)token).getDefaultValue().split(",");
+					
+					//boolean captureOutput = params.length>1 && params[1].equals("true");
+					
+					Object result = _controller.widget(((StringToken)token).getDefaultValue(), true);
+					return result==null||result.equals("") ? new NullOutput() : result;
+				}
+			} catch (InstantiationException e)
+			{
+				throw new RuntimeException(e);
+			} catch (IOException e)
+			{
+				throw new RuntimeException(e);
+			}
+			return null;
+		}
+	}
+	
+	
 }
